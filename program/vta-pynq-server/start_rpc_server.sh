149058e090fdae053c501c1fd141848e7926da65
Xilinx User <xilinx@pynq>
2018-04-06 16:11:57 +0000

    fixed server script

--- program/vta-pynq-server/start_rpc_server.sh
+++ program/vta-pynq-server/start_rpc_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo $PYTHONPATH
+
+echo "********************************************************************************"
+echo "Local IP:"
+echo ""
+
+ifconfig eth0
+echo "********************************************************************************"
+
 python -m  tvm.exec.rpc_server --load-library ${CK_ENV_LIB_VTA_SERVER_LIB_FULL}
